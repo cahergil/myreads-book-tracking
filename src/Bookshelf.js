d7a524cdaff029efe466f31e36a78a56c346dc7a
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-08-22 17:54:27 +0200

    feat: Add basic funcionality

---
+++ src/Bookshelf.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+
+class Bookshelf extends Component {
+
+
+
+
+
+
+
+    render() {
+         console.log(this.props);
+         let books = this.props.list.bookList;
+         const shelf = this.props.shelf;
+         const onMoveBook = this.props.onMoveBook;
+
+         books = books.filter((book) =>book.shelf === shelf);
+
+        return (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{shelf}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+             { books.map( (book) => (
+                     <li key={book.name}>
+                       <div className="book">
+                         <div className="book-top">
+                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
+                           <div className="book-shelf-changer">
+                             <select onChange={(e) => onMoveBook(book,e.target.value) }>
+                               <option value="move" disabled>Move to...</option>
+                               <option value="currentlyReading">Currently Reading</option>
+                               <option value="wantToRead">Want to Read</option>
+                               <option value="read">Read</option>
+                               <option value="none">None</option>
+                             </select>
+                           </div>
+                         </div>
+                         <div className="book-title">{book.title}</div>
+                         <div className="book-authors">{book.authors}</div>
+                       </div>
+                     </li>
+                 )
+             )
+             }
+
+
+
+            </ol>
+          </div>
+        </div>
+
+        )
+    }
+
+
+
+}
+
+export default Bookshelf
