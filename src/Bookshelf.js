abc39068fa181994187b60e772704c68fe8af99d
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-08-27 12:42:11 +0200

    feat: Add empty shelf status

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -32,35 +32,43 @@ class Bookshelf extends Component {
         <div className="bookshelf">
           <h2 className="bookshelf-title">{this.mapShelves(shelf)}</h2>
           <div className="bookshelf-books">
-            <ol className="books-grid">
-             { books.map( (book) => (
-                     <li key={book.title}>
-                       <div className="book">
-                         <div className="book-top">
-                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:
-                                 `url(${book.imageLinks? book.imageLinks.smallThumbnail:''} )` }}></div>
-                           <div className="book-shelf-changer">
-                             <select id="selector" onChange={(e) => onMoveBook(book,e.target.value) } value={shelf} >
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead" >Want to Read</option>
-                                    <option value="read" >Read</option>
-                                    <option value="none" >None</option>
+             { books.length === 0 ? (
 
-                             </select>
-                           </div>
-                         </div>
-                         <div className="book-title">{book.title}</div>
-                         <div className="book-authors">{book.authors ? book.authors.join(',') : 'not available'}</div>
-                       </div>
-                     </li>
+                    <h3 className="bookshelf-status">Empty Shelf</h3>
+             ) : (
+
+                    <ol className="books-grid">
+                      {
+                        books.map( (book) => (
+                             <li key={book.title}>
+                               <div className="book">
+                                 <div className="book-top">
+                                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:
+                                         `url(${book.imageLinks? book.imageLinks.smallThumbnail:''} )` }}></div>
+                                   <div className="book-shelf-changer">
+                                     <select id="selector" onChange={(e) => onMoveBook(book,e.target.value) } value={shelf} >
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead" >Want to Read</option>
+                                            <option value="read" >Read</option>
+                                            <option value="none" >None</option>
+
+                                     </select>
+                                   </div>
+                                 </div>
+                                 <div className="book-title">{book.title}</div>
+                                 <div className="book-authors">{book.authors ? book.authors.join(',') : 'not available'}</div>
+                               </div>
+                             </li>
+                           ))
+                      }
+                     </ol>
                  )
-             )
              }
 
 
 
-            </ol>
+
           </div>
         </div>
 
