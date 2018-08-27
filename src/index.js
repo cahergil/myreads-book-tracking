d57d2a98d071fb82eedfc6f82822b918f2b9049a
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-08-27 19:26:10 +0200

    fix: Add correct import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 //import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     //<BrowserRouter><App /></BrowserRouter>,
